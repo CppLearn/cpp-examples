ed683ba3555b5d4ec019c504034cb0813b8ef9d6
Circuit <circuit.nirvana@gmail.com>
2023-03-21 22:27:53 -0600

    updating files...

--- cpp/update-files.sh
+++ cpp/update-files.sh
@@ -1,16 +1,42 @@
 #!/usr/bin/sh
 
+#
+# Quick Cpp
+#
 echo "updating quickcpp.cpp..."
 cp -pv ~/git/cpp/QuickCPP/quickcpp.cpp .
 echo "building quickcpp..."
 make quickcpp
 git add quickcpp.cpp
+
+#
+# Timer
+#
+echo "updating Timer..."
+cp -pv ~/git/cpp/NanoTimer/nanotimer.hpp
+cp -pv ~/git/cpp/NanoTimer/nanotimer.cpp
+cp -pv ~/git/cpp/NanoTimer/nano_main.cpp
+cp -pv ~/git/cpp/NanoTimer/makefile
+
+#
+# C++ quick files
+#
 cp -rpv ~/git/cpp/quick/*.cc quick
 git add quick/*.cc
+
+#
+# DOT graph files
+#
 cp -rpv ~/git/micros/graphviz/*.dot ../dot
 git add ../dot/*.dot
+
+#
+# CL libs
+#
 cp -rpv ~/git/lisp/libs/* ../cl/libs
 git add ../cl/libs/*
+
+
 git commit -a -m "updating files..."
 
 
