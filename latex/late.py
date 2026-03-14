0817ae40d29a1269817c5c29ac85004f86c59fed
Circuit <circuit.nirvana@gmail.com>
2026-03-14 00:39:46 -0600

    adding raylib slides

--- latex/late.py
+++ latex/late.py
@@ -3,20 +3,14 @@
 import os, sys
 
 fname = sys.argv[1]
-dvi = '%s.dvi' % fname
-ps  = '%s.ps' % fname
+pdf  = '%s.pdf' % fname
 
-if os.path.exists(dvi):
-    os.unlink(dvi)
-if os.path.exists(ps):
-    os.unlink(ps)
+if os.path.exists(pdf):
+    os.unlink(pdf)
 
-cmd = 'latex %s' % fname
+cmd = 'pdflatex %s' % fname
 os.system(cmd)
-cmd = 'dvips %s' % fname
+cmd = 'evince %s.pdf' % fname
 os.system(cmd)
-cmd = 'ps2pdf %s.ps' % fname
-os.system(cmd)
-cmd = 'evince %s.ps' % fname
-os.system(cmd)
+
 
